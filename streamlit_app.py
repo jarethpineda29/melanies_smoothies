0cf477f61742ca7d241ff3f1dce701e075284782
jarethpineda29 <jareth.pineda@snowflake.com>
2026-05-27 15:22:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ if ingredients_list:
 
         time_to_insert = st.button ('Submit Order')
       
-      if time_to_insert:
+    if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
